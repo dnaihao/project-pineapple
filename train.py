d482fb66f3d6f7c86725785a22e11689c3f80a37
Fuhu Xiao <fuhu@umich.edu>
2020-02-13 20:11:44 -0500

    cnn da wan le

---
+++ train.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+import json
+import os
+from PIL import Image
+
+from VRUDataset import VRUDataset
+
+def train():
+    train_set = VRUDataset();
+
+
+
+
+
+
+if __name__ == "__main__":
+    train()
