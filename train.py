bd2bf2a54cc4745cd79ba46880bc11a0be78cc31
Fuhu Xiao <fuhu@umich.edu>
2020-02-13 19:58:03 -0500

    placeholder

---
+++ train.py
@@ -0,0 +1,34 @@
+from __future__ import print_function
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+import time
+import json
+import os
+from PIL import Image
+
+class VRUDataset(Dataset):
+    def __init__(self, json_path="new.json", data_path="done"):
+        with open(json_path) as f:
+            self.json = json.load(f)
+        self.data_path = data_path
+
+    def __len__(self):
+        return len(self.json['obj'])
+    
+    def __getitem__(self, idx):
+        img_name = self.json['obj'][idx]['f_name']
+        img = Image.open(os.path.join(self.data_path, img_name))
+        return {
+            'image': img,
+            'label': self.json['obj'][idx]['label']
+        }
+
+
+if __name__ == "__main__":
+    V = VRUDataset()
+    import ipdb; ipdb.set_trace()
+    a = 1
