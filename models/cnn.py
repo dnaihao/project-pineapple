e57ecdbdb6ad3287fe2a80bdf0f104969d79fd0b
Fuhu Xiao <fuhu@umich.edu>
2020-02-13 22:40:52 -0500

    batch size not match

--- models/cnn.py
+++ models/cnn.py
@@ -1,20 +1,21 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
-class Net(nn.Module):
+class CNN(nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(4, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc1 = nn.Linear(15488, 120)
         self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.fc3 = nn.Linear(84, 2)
 
     def forward(self, x):
+        # import ipdb; ipdb.set_trace()
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
+        x = x.view(-1, 15488)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
--- train.py
+++ train.py
@@ -5,15 +5,56 @@ import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
+import torch.optim as optim
 import json
 import os
+from sklearn.preprocessing import OneHotEncoder
 from PIL import Image
+import numpy as np
 
 from VRUDataset import VRUDataset
+from models.cnn import CNN
+
+EPOCH = 2
+
 
 def train():
-    train_set = VRUDataset();
+    transform = transforms.Compose([transforms.ToTensor()])
+    train_set = VRUDataset(transform=transform)
+    train_set = DataLoader(train_set, batch_size=4, shuffle=4, num_workers=4)
 
+    cnn = CNN()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(cnn.parameters(), lr=0.001, momentum=0.9)
+    enc = OneHotEncoder()
+    # train
+    for epoch in range(EPOCH):  # loop over the dataset multiple times
+        running_loss = 0.0
+        for i, data in enumerate(train_set, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs = data.get("image")
+            labels = data.get("label")
+            labels = np.array(labels).reshape(-1, 1)
+            print(labels)
+            enc.fit(labels)
+            encodedLabels = enc.transform(labels).toarray()
+            encodedLabels = torch.Tensor(encodedLabels)
+            # import ipdb; ipdb.set_trace()
+            optimizer.zero_grad()
+            # forward + backward + optimize
+            outputs = cnn(inputs)
+            # import ipdb; ipdb.set_trace()
+            loss = criterion(outputs, encodedLabels)
+            loss.backward()
+            optimizer.step()
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                    (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+    print('Finished Training')
+    
 
 
 
