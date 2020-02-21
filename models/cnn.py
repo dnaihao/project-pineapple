88d57a161be18943fdad9fb243cac5a6fd443df5
Fuhu Xiao <fuhu@umich.edu>
2020-02-20 20:48:18 -0500

    blueberry

--- models/cnn.py
+++ models/cnn.py
@@ -7,7 +7,7 @@ class CNN(nn.Module):
         self.conv1 = nn.Conv2d(4, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(7744, 120)
+        self.fc1 = nn.Linear(15488, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 2)
 
@@ -16,7 +16,7 @@ class CNN(nn.Module):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
         print(x.shape)
-        x = x.view(-1, 7744)
+        x = x.view(-1, 15488)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
