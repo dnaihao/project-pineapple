90ab4d87585c23d6458c8bf2adacd7caf8fb1223
Fuhu Xiao <fuhu@umich.edu>
2020-02-20 19:41:21 -0500

    ??

--- models/cnn.py
+++ models/cnn.py
@@ -7,7 +7,7 @@ class CNN(nn.Module):
         self.conv1 = nn.Conv2d(4, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(15488, 120)
+        self.fc1 = nn.Linear(7744, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 2)
 
@@ -15,7 +15,8 @@ class CNN(nn.Module):
         # import ipdb; ipdb.set_trace()
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 15488)
+        print(x.shape)
+        x = x.view(-1, 7744)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
