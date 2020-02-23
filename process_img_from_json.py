d22e8cb055d920b9a01e63557c96da8d49240f1e
Fuhu Xiao <fuhu@umich.edu>
2020-02-22 20:59:54 -0500

    split train and validation

--- process_img_from_json.py
+++ process_img_from_json.py
@@ -4,9 +4,11 @@ from PIL import Image
 import argparse
 
 RAW_DATA_PATH = "C:\\Users\\shubh\\OneDrive\\Desktop\\ENTR 390\\Dataset\\Images_RGB"
-PROCESSED_DATA_PATH = "C:\\Users\\shubh\\OneDrive\\Desktop\\ENTR 390\\Dataset\\Im"
+PROCESSED_TRAIN_DATA_PATH = "C:\\Users\\shubh\\OneDrive\\Desktop\\ENTR 390\\Dataset\\Train"
+PROCESSED_VAL_DATA_PATH = "C:\\Users\\shubh\\OneDrive\\Desktop\\ENTR 390\\Dataset\\Val"
 JSON_F = "C:\\Users\\shubh\\OneDrive\\Desktop\\ENTR 390\\Dataset\\Mobility.json"
 NEW_SHAPE = (100, 100)
+TRAIN_TO_VAL_RATIO = 0.01
 
 
 def gen_json():
@@ -30,42 +32,65 @@ def gen_json():
     }
     return json_f
 
-def write_json(j):
-    print("write json file...")
-    with open('new.json', 'w') as f:
-        json.dump(j, f)
+def write_json(j, train=True):
+    if train:
+        print("write train json file...")
+        with open('train.json', 'w') as f:
+            json.dump(j, f)
+    else:
+        print("write val json file...")
+        with open('val.json', 'w') as f:
+            json.dump(j, f)
 
 # process images and create updated json file
 def process_images():
-    new_j = {'obj':[]}
+    train_j = {'obj':[]}
+    val_j = {'obj':[]}
     j = {}
     with open(JSON_F, 'r') as f:
         j = json.load(f)
     index = 0
-    print(j)
+    divide = len(j['obj']) * TRAIN_TO_VAL_RATIO
     for idx, obj in enumerate(j['obj']):
-        if obj['label'] != 'NA':
-            region = (obj['dim'][1], obj['dim'][3], obj['dim'][0], obj['dim'][2])
-            f_name = crop_and_resize_image(obj['f_name'], region=region, index=index)
-            obj['f_name'] = f_name
-            new_j['obj'].append({
-                'f_name': f_name,
-                'label': obj['label'],
-                'size_x': obj['size_x'],
-                'size_y': obj['size_y'],
-                'dim': region
-                })
-            index += 1
-        if (idx % 100 == 0):
-            write_json(new_j)
-    write_json(new_j)
+        if (idx < divide):
+            if obj['label'] != 'NA':
+                region = (obj['dim'][1], obj['dim'][3], obj['dim'][0], obj['dim'][2])
+                f_name = crop_and_resize_image(obj['f_name'], region=region, index=index, train=true)
+                obj['f_name'] = f_name
+                train_j['obj'].append({
+                    'f_name': f_name,
+                    'label': obj['label'],
+                    'size_x': obj['size_x'],
+                    'size_y': obj['size_y'],
+                    'dim': region
+                    })
+                index += 1
+            if (idx % 100 == 0):
+                write_json(train_j, train=True)
+        else:
+            if obj['label'] != 'NA':
+                region = (obj['dim'][1], obj['dim'][3], obj['dim'][0], obj['dim'][2])
+                f_name = crop_and_resize_image(obj['f_name'], region=region, index=index, train=False)
+                obj['f_name'] = f_name
+                val_j['obj'].append({
+                    'f_name': f_name,
+                    'label': obj['label'],
+                    'size_x': obj['size_x'],
+                    'size_y': obj['size_y'],
+                    'dim': region
+                    })
+                index += 1
+            if (idx % 100 == 0):
+                write_json(val_j, train=False)
         
-def crop_and_resize_image(f_name, region, index):
+def crop_and_resize_image(f_name, region, index, train=True):
     img_path = os.path.join(os.getcwd(), RAW_DATA_PATH, f_name)
     if f_name.endswith('.jpg') or f_name.endswith('.png'):
         f_name = f_name[:-4] + '_' + str(index) + f_name[-4:]
-
-    save_path = os.path.join(os.getcwd(), PROCESSED_DATA_PATH, f_name)
+    if train:
+        save_path = os.path.join(os.getcwd(), PROCESSED_TRAIN_DATA_PATH, f_name)
+    else:
+        save_path = os.path.join(os.getcwd(), PROCESSED_VAL_DATA_PATH, f_name)
     try:
         img = Image.open(img_path)
         img = img.crop(region).resize(NEW_SHAPE)
@@ -76,7 +101,7 @@ def crop_and_resize_image(f_name, region, index):
     return f_name
 
 def parse_args():
-    global RAW_DATA_PATH, PROCESSED_DATA_PATH, JSON_F, NEW_SHAPE
+    global RAW_DATA_PATH, PROCESSED_TRAIN_DATA_PATH, PROCESSED_VAL_DATA_PATH, JSON_F, NEW_SHAPE
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", "--shape", nargs=2, default=(32, 32), required=False)
     parser.add_argument("-j", "--json", nargs='*', default="my.json", required=False)
