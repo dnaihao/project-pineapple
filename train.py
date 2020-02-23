7d0554d66b42bd6e9ff2164edc6b2c1e37ab5da9
shubhang <shubhang@umich.edu>
2020-02-22 23:12:10 -0500

    Training works

--- train.py
+++ train.py
@@ -8,21 +8,23 @@ from torch.utils.data import Dataset, DataLoader
 import torch.optim as optim
 import json
 import os
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 from PIL import Image
 import numpy as np
-
+from sklearn.metrics import accuracy_score
 from VRUDataset import VRUDataset
 from models.cnn import CNN
 
-EPOCH = 3
+EPOCH = 2
 
 
 def train():
     transform = transforms.Compose([transforms.ToTensor()])
-    train_set = VRUDataset(transform=transform,data_path="C:\\Users\\shubh\\OneDrive\\Desktop\\ENTR 390\\Dataset\\Train")
-    val_set = VRUDataset(transform=transform,data_path="C:\\Users\\shubh\\OneDrive\\Desktop\\ENTR 390\\Dataset\\Val")
+    train_set = VRUDataset(transform=transform,json_path="train.json",data_path="C:\\Users\\shubh\\OneDrive\\Desktop\\ENTR 390\\Dataset\\Train")
+    val_set = VRUDataset(transform=transform,json_path="val.json",data_path="C:\\Users\\shubh\\OneDrive\\Desktop\\ENTR 390\\Dataset\\Val")
     train_set = DataLoader(train_set,shuffle=True)
+    val_set = DataLoader(val_set,shuffle=True)
 
     cnn = CNN()
     # criterion = nn.CrossEntropyLoss()
@@ -30,6 +32,12 @@ def train():
     optimizer = optim.SGD(cnn.parameters(), lr=0.001, momentum=0.9)
     # enc = OneHotEncoder()
     enc = LabelEncoder()
+    classes=['wheelchair','walking_frame','crutches','person','push_wheelchair']
+    enc.fit(classes)
+    # empty list to store training losses
+    train_losses = []
+    # empty list to store validation losses
+    val_losses = []
     #cuda = torch.device('cuda')
     # train
     for epoch in range(EPOCH):  # loop over the dataset multiple times
@@ -43,30 +51,41 @@ def train():
             #if torch.cuda.is_available():
                 #inputs = torch.tensor(inputs).cuda()
                 #labels = torch.tensor(labels).cuda()
-                #x_val = x_val.cuda()
-                #y_val = y_val.cuda()
-            #print(labels)
-            enc.fit(labels)
+            
             encodedLabels = enc.transform(labels)
+
             encodedLabels = torch.Tensor(encodedLabels).unsqueeze(0)
             # import ipdb; ipdb.set_trace()
             optimizer.zero_grad()
+            #if torch.cuda.is_available():
+                #cnn=cnn.cuda()
+                #criterion=criterion.cuda()
             # forward + backward + optimize
             #print(inputs.shape)
             train_outputs = cnn(inputs)
             
             #print(outputs.shape)
             # import ipdb; ipdb.set_trace()
-            #print(outputs.shape, encodedLabels.shape)
-            loss = criterion(outputs, encodedLabels)
+            loss = criterion(train_outputs, encodedLabels)
+            train_losses.append(loss)
             loss.backward()
             optimizer.step()
             # print statistics
             running_loss += loss.item()
-            if i % 200 == 199:    # print every 2000 mini-batches
+            if i % 1000 == 999:    # print every 2000 mini-batches
                 print('[%d, %5d] train loss: %.3f' %
-                    (epoch + 1, i + 1, running_loss / 200))
+                    (epoch + 1, i + 1, running_loss / 1000))
                 running_loss = 0.0
+                #softmax=torch.exp(train_outputs)
+                #prob=list(softmax.numpy())
+                #train_pred=np.argmax(prob,axis=1)
+                #train_accuracy=accuracy_score(labels,train_pred)
+                #print("Training Accuracy = ",train_accuracy)
+                plt.plot(train_losses, label='Training loss')
+                plt.legend()
+                plt.show()
+
+
         running_loss=0.0
         for i, data in enumerate(val_set,0):
             inputs = data.get("image")
@@ -75,17 +94,30 @@ def train():
             #if torch.cuda.is_available():
                 #inputs = torch.tensor(inputs).cuda()
                 #labels = torch.tensor(labels).cuda()
-            enc.fit(labels)
+                #cnn=cnn.cuda()
+                #criterion=criterion.cuda()
+
+            #enc.fit(labels)
             encodedLabels = enc.transform(labels)
             val_encodedLabels = torch.Tensor(encodedLabels).unsqueeze(0)
             val_outputs=cnn(inputs)
             val_loss=criterion(val_outputs,val_encodedLabels)
             running_loss+= val_loss.item()
-            if i % 200 == 199:    # print every 2000 mini-batches
+            val_losses.append(val_loss)
+            if i % 1000 == 199:    # print every 2000 mini-batches
                 print('[%d, %5d] validation loss: %.3f' %
-                    (epoch + 1, i + 1, running_loss / 200))
+                    (epoch + 1, i + 1, running_loss / 1000))
                 running_loss = 0.0
+                #softmax=torch.exp(val_outputs)
+                #prob=list(softmax.numpy())
+                #val_pred=np.argmax(prob,axis=1)
+                #val_accuracy=accuracy_score(labels,val_pred)
+                #print("Validation Accuracy = ",val_accuracy)
+                plt.plot(val_losses, label='Validation loss')
+                plt.legend()
+                plt.show()
             
+
     print('Finished Training')
     
 
